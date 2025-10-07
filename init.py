4fa8206bce90b93c88e857e460d280dbfb4cff28
danielfce01 <danielf.carmona@udea.edu.co>
2025-10-06 22:39:09 -0500

    Update init.py

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 course_id = 'Taller-1-Data-Streaming'
 github_repo = 'danielfce01/%s' % course_id
-zip_file_url = "https://github.com/%s/archive/refs/heads/main.zip" % github_repo
+zip_file_url = "https://github.com/%s/archive/master.zip" % github_repo
 
 def get_last_modif_date(localdir):
     try:
@@ -12,31 +12,19 @@ def get_last_modif_date(localdir):
         return k
     except Exception:
         return None
-
+    
 import requests, zipfile, io, os, shutil
-
 def init(force_download=False):
     if force_download or not os.path.exists("local"):
-        print("⬇️ Replicando recursos del repositorio...")
-        dirname = course_id + "-main/"
-
+        print("replicating local resources")
+        dirname = course_id+"-master/"
         if os.path.exists(dirname):
             shutil.rmtree(dirname)
-
         r = requests.get(zip_file_url)
         z = zipfile.ZipFile(io.BytesIO(r.content))
         z.extractall()
-
         if os.path.exists("local"):
             shutil.rmtree("local")
-
-        shutil.move(dirname + "/local", "local")
-
-        if os.path.exists("gen-py"):
-            shutil.rmtree("gen-py")
-        shutil.move(dirname + "/gen-py", "gen-py")
-
+        shutil.move(dirname+"/local", "local")
         shutil.rmtree(dirname)
-        print("✅ Carpetas 'local' y 'gen-py' listas para usar.")
-    else:
-        print("✅ Recursos locales ya disponibles.")
+
