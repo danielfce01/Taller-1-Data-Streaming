a14d2ae22cf28ffd28b92ebcc47f85495722a892
danielfce01 <danielf.carmona@udea.edu.co>
2025-10-06 21:59:55 -0500

    Add files via upload

---
+++ local/data/ventas_garantias.thrift
@@ -0,0 +1,13 @@
+struct Venta {
+  1: i32 id,
+  2: string comprador,
+  3: i64 cedula,
+  4: string categoria,
+  5: string producto,
+  6: i64 precio,
+  7: string metodo_pago,
+  8: string fecha_venta,
+  9: string fecha_garantia,
+ 10: string solucion,
+ 11: string queja
+}
